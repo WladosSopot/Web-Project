3e4b4a64714e91376e885a61e76854f2a381c6ec
vladopot <vladsopot@mail.ru>
2026-02-12 13:23:15 +0100

    added_axios

--- beck-end/src/user/user.controller.ts
+++ beck-end/src/user/user.controller.ts
@@ -1,34 +1,47 @@
-import { Controller, Get, Post, Param, Delete, Body, ParseIntPipe, ValidationPipe, Patch } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Param,
+  Delete,
+  Body,
+  ParseIntPipe,
+  ValidationPipe,
+  Patch,
+} from '@nestjs/common';
 import { UserService } from './user.service.js';
 import { CreateUserDto } from './dto/create-user.dto.js';
 import { UpdateUserDto } from './dto/update-user.dto.js';
 
 @Controller('user')
 export class UserController {
-    constructor(private readonly userService: UserService){}
+  constructor(private readonly userService: UserService) {}
 
-    @Get()
-    findAll(){
-        return this.userService.findAll()
-    }
+  @Get()
+  findAll() {
+    return this.userService.findAll();
+  }
 
-    @Get(':id')
-    findOne(@Param('id', ParseIntPipe) id: number){
-        return this.userService.findById(id);
-    }
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.findById(id);
+  }
 
-    @Post()
-    create(@Body(ValidationPipe) createUserDto: CreateUserDto){
-        return this.userService.create(createUserDto);
-    }
+  @Post()
+  create(@Body(ValidationPipe) createUserDto: CreateUserDto) {
+    return this.userService.create(createUserDto);
+  }
 
-    @Patch(':id')
-    update(@Param('id', ParseIntPipe) id: number, @Body(ValidationPipe) updateUserDto: UpdateUserDto){
-        return this.userService.update(id, updateUserDto);
-    }
+  @Patch(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body(ValidationPipe) updateUserDto: UpdateUserDto,
+  ) {
+    return this.userService.update(id, updateUserDto);
+  }
 
-    @Delete(':id')
-    delete(@Param('id', ParseIntPipe) id: number){
-        return this.userService.delete(id);
-    }
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.userService.delete(id);
+  }
 }
